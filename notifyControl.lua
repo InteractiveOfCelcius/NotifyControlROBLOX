2426cd1d17acf3d67f1c0cd41447970109026444
psvks <134863563+InteractiveOfCelcius@users.noreply.github.com>
2023-12-20 19:46:41 +0100

    Made notification managment.
    Now can handle more than 1 notification at a time, and duration

--- notifyControl.lua
+++ notifyControl.lua
@@ -1,211 +1,116 @@
-local NotifyCore = {}
+local notifyroblox = {}
+	function notifyroblox:MakeInstance(plr) -- INSTANCE #<i9d213Ds11>, Instance for the notifys.
+		local ScreenGui = Instance.new("ScreenGui")
+		local NotifyBox = Instance.new("Frame")
+		local UIListLayout = Instance.new("UIListLayout")
 
+		ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+		ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
+		NotifyBox.Name = "NotifyBox"
+		NotifyBox.Parent = ScreenGui
+		NotifyBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		NotifyBox.BackgroundTransparency = 1.000
+		NotifyBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+		NotifyBox.BorderSizePixel = 0
+		NotifyBox.Position = UDim2.new(0.726446271, 0, 0, 0)
+		NotifyBox.Size = UDim2.new(0.268049806, 0, 0.987215936, 0)
 
-function NotifyCore:CreateNotify(titled, ddesc, callback)
-	print('Made notification')
-	callback = callback or function() end
-	titled = titled or "Untitled"
-	ddesc = ddesc or "No description was added:48"
-	local NotifyControl = Instance.new("ScreenGui")
-	local NotifyMain = Instance.new("Frame")
-	local UICorner = Instance.new("UICorner")
-	local separao = Instance.new("TextLabel")
-	local title = Instance.new("TextLabel")
-	local desc = Instance.new("TextLabel")
-	local okButton = Instance.new("TextButton")
-	local UICorner_2 = Instance.new("UICorner")
+		UIListLayout.Parent = NotifyBox
+		UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+		UIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
+		UIListLayout.Padding = UDim.new(0, 10)
+		local InstanceN = {}
+			function InstanceN.Notify(title, description, duration) -- NOTIFICATION #<n2aD2433FA211D>, Normal Notification without callback.
+				local OnlyOneButton = Instance.new("Frame")
+				local Title = Instance.new("TextLabel")
+				local Description = Instance.new("TextLabel")
+				local TextButton = Instance.new("TextButton")
+				local timer = Instance.new("TextLabel")
 
-	NotifyControl.Name = "NotifyControl"
-	NotifyControl.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-	NotifyControl.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+				OnlyOneButton.Name = "OnlyOneButton"
+				OnlyOneButton.Parent = NotifyBox
+				OnlyOneButton.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
+				OnlyOneButton.BackgroundTransparency = 0.300
+				OnlyOneButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+				OnlyOneButton.Position = UDim2.new(0, 0, 0.871942461, 0)
+				OnlyOneButton.Size = UDim2.new(0.999999881, 0, 0.128057554, 0)
 
-	NotifyMain.Name = "NotifyMain"
-	NotifyMain.Parent = NotifyControl
-	NotifyMain.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
-	NotifyMain.Position = UDim2.new(0.00694638537, 0, 0.0111111104, 0)
-	NotifyMain.Size = UDim2.new(0, 270, 0, 124)
+				Title.Name = "Title"
+				Title.Parent = OnlyOneButton
+				Title.BackgroundColor3 = Color3.fromRGB(43, 43, 43)
+				Title.BackgroundTransparency = 0.150
+				Title.BorderColor3 = Color3.fromRGB(0, 0, 0)
+				Title.Size = UDim2.new(1, 0, 0.179775283, 0)
+				Title.Font = Enum.Font.Code
+				Title.Text = title
+				Title.TextColor3 = Color3.fromRGB(143, 143, 143)
+				Title.TextScaled = true
+				Title.TextSize = 14.000
+				Title.TextWrapped = true
+				Title.TextXAlignment = Enum.TextXAlignment.Left
 
-	UICorner.CornerRadius = UDim.new(0, 3)
-	UICorner.Parent = NotifyMain
+				Description.Name = "Description"
+				Description.Parent = OnlyOneButton
+				Description.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+				Description.BackgroundTransparency = 1.000
+				Description.BorderColor3 = Color3.fromRGB(0, 0, 0)
+				Description.BorderSizePixel = 0
+				Description.Position = UDim2.new(0.0181268752, 0, 0.179775283, 0)
+				Description.Size = UDim2.new(0.978328168, 0, 0.57303369, 0)
+				Description.Font = Enum.Font.Code
+				Description.TextColor3 = Color3.fromRGB(255, 255, 255)
+				Description.TextSize = 14.000
+				Description.TextWrapped = true
+				Description.TextXAlignment = Enum.TextXAlignment.Left
+				Description.TextYAlignment = Enum.TextYAlignment.Top
+				Description.TextScaled = true
+				Description.Text = description
 
-	separao.Name = "separao"
-	separao.Parent = NotifyMain
-	separao.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	separao.BorderColor3 = Color3.fromRGB(255, 0, 127)
-	separao.Position = UDim2.new(0, 0, 0.169354841, 0)
-	separao.Size = UDim2.new(0, 270, 0, 0)
-	separao.Font = Enum.Font.SourceSans
-	separao.Text = " "
-	separao.TextColor3 = Color3.fromRGB(0, 0, 0)
-	separao.TextSize = 14.000
+				TextButton.Parent = OnlyOneButton
+				TextButton.BackgroundColor3 = Color3.fromRGB(21, 21, 21)
+				TextButton.BackgroundTransparency = 0.500
+				TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+				TextButton.BorderSizePixel = 0
+				TextButton.Position = UDim2.new(0.829996467, 0, 0.756894886, 0)
+				TextButton.Size = UDim2.new(0.167182669, 0, 0.23595506, 0)
+				TextButton.Font = Enum.Font.SourceSans
+				TextButton.Text = "OK"
+				TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+				TextButton.TextSize = 14.000
 
+				timer.Name = "timer"
+				timer.Parent = OnlyOneButton
+				timer.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+				timer.BackgroundTransparency = 1.000
+				timer.BorderColor3 = Color3.fromRGB(0, 0, 0)
+				timer.BorderSizePixel = 0
+				timer.Position = UDim2.new(0, 0, 0.752808988, 0)
+				timer.Size = UDim2.new(0.0681114569, 0, 0.247191012, 0)
+				timer.Font = Enum.Font.SourceSans
+				timer.Text = "69"
+				timer.TextColor3 = Color3.fromRGB(162, 162, 162)
+				timer.TextScaled = true
+				timer.TextSize = 14.000
+				timer.TextWrapped = true
 
+				local numLines = description:match("\n") and #description:split("\n") + 1 or 1
+				OnlyOneButton.Size = UDim2.new(0.999999881, 0, numLines * 0.05, 0)
 
-	title.Name = "title"
-	title.Parent = NotifyMain
-	title.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
-	title.BackgroundTransparency = 1.000
-	title.Position = UDim2.new(0.025925925, 0, 0, 0)
-	title.Size = UDim2.new(0, 193, 0, 21)
-	title.Font = Enum.Font.Code
-	title.Text = "Untitled"
-	title.TextColor3 = Color3.fromRGB(255, 255, 255)
-	title.TextSize = 14.000
-	title.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
-	title.TextXAlignment = Enum.TextXAlignment.Left
+				local timeLeft = duration or 10
+				timer.Text = tostring(timeLeft)
+			
+				TextButton.MouseButton1Click:Connect(function()
+					OnlyOneButton:Destroy()
+				end)
+			
+				for i = timeLeft, 1, -1 do
+					wait(1)
+					timer.Text = tostring(i)
+				end
+				OnlyOneButton:Destroy()
+			end
+		return InstanceN
+	end
 
-
-
-	desc.Name = "desc"
-	desc.Parent = NotifyMain
-	desc.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
-	desc.BackgroundTransparency = 1.000
-	desc.Position = UDim2.new(0.0111111114, 0, 0.217741936, 0)
-	desc.Size = UDim2.new(0, 261, 0, 72)
-	desc.Font = Enum.Font.Code
-	desc.TextColor3 = Color3.fromRGB(255, 0, 127)
-	desc.TextSize = 14.000
-	desc.TextWrapped = true
-	desc.TextXAlignment = Enum.TextXAlignment.Left
-	desc.TextYAlignment = Enum.TextYAlignment.Top
-
-	title.Text = titled
-	title.TextSize = 14
-
-	desc.Text = ddesc
-	desc.TextSize = 14
-	desc.TextWrapped = true
-	desc.Size = UDim2.new(0, 261, 0, desc.TextBounds.Y)
-	desc.TextYAlignment = Enum.TextYAlignment.Top
-
-	okButton.Text = "OK."
-	okButton.Visible = true
-
-	NotifyMain.Size = UDim2.new(0, 270, 0, desc.TextBounds.Y + 100)
-
-
-	okButton.Name = "okButton"
-	okButton.Parent = NotifyMain
-	okButton.BackgroundColor3 = Color3.fromRGB(255, 0, 127)
-	okButton.Position = UDim2.new(0.688888907, 0, 0.79838711, 0)
-	okButton.Size = UDim2.new(0, 78, 0, 18)
-	okButton.Font = Enum.Font.SourceSans
-	okButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-	okButton.TextSize = 14.000
-
-	UICorner_2.CornerRadius = UDim.new(0, 3)
-	UICorner_2.Parent = okButton
-
-
-
-	UICorner_2.CornerRadius = UDim.new(0, 3)
-	UICorner_2.Parent = okButton
-
-	local UISTROK = Instance.new("UIStroke")
-
-	UISTROK.Parent = NotifyMain
-	UISTROK.Color = Color3.fromRGB(255, 0, 127)
-	UISTROK.Thickness = 1
-
-
-	okButton.Text = "OK."
-	okButton.Font = Enum.Font.Code
-
-
-	--- INSTANCES
-	local speed = 0.01
-	title.Text = titled
-	desc.Text = ddesc
-	okButton.Visible = true
-	NotifyMain.Visible = true
-	local sound = Instance.new("Sound", game.Workspace)
-	sound.SoundId = "rbxassetid://654933978"
-	sound.Volume = 5
-	sound:Play()
-	okButton.MouseButton1Click:Connect(function()
-		okButton.Visible = false
-		NotifyMain.BackgroundTransparency = 0.1
-		title.TextTransparency = 0.1
-		desc.TextTransparency = 0.1
-		separao.BackgroundTransparency = 0.1
-		wait(speed)
-
-
-		NotifyMain.BackgroundTransparency = 0.2
-		title.TextTransparency = 0.2
-		desc.TextTransparency = 0.2
-		separao.BackgroundTransparency = 0.2
-		wait(speed)
-
-
-		NotifyMain.BackgroundTransparency = 0.3
-		title.TextTransparency = 0.3
-		desc.TextTransparency = 0.3
-		separao.BackgroundTransparency = 0.3
-		wait(speed)
-
-
-		NotifyMain.BackgroundTransparency = 0.4
-		title.TextTransparency = 0.4
-		desc.TextTransparency = 0.4
-		separao.BackgroundTransparency = 0.4
-		wait(speed)
-
-
-		NotifyMain.BackgroundTransparency = 0.5
-		title.TextTransparency = 0.5
-		desc.TextTransparency = 0.5
-		separao.BackgroundTransparency = 0.5
-		wait(speed)
-
-
-		NotifyMain.BackgroundTransparency = 0.6
-		title.TextTransparency = 0.6
-		desc.TextTransparency = 0.6
-		separao.BackgroundTransparency = 0.6
-		wait(speed)
-
-
-		NotifyMain.BackgroundTransparency = 0.7
-		title.TextTransparency = 0.7
-		desc.TextTransparency = 0.7
-		separao.BackgroundTransparency = 0.7
-		wait(speed)
-
-
-		NotifyMain.BackgroundTransparency = 0.8
-		title.TextTransparency = 0.8
-		desc.TextTransparency = 0.8
-		separao.BackgroundTransparency = 0.8
-		wait(speed)
-
-
-		NotifyMain.BackgroundTransparency = 0.9
-		title.TextTransparency = 0.9
-		desc.TextTransparency = 0.9
-		separao.BackgroundTransparency = 0.9
-		wait(speed)
-
-
-		NotifyMain.BackgroundTransparency = 1
-		title.TextTransparency = 1
-		desc.TextTransparency = 1
-		separao.BackgroundTransparency = 1
-
-		NotifyMain.Visible = false
-		wait(.1)
-		NotifyMain.BackgroundTransparency = 0
-		title.TextTransparency = 0
-		desc.TextTransparency = 0
-		separao.BackgroundTransparency = 0
-		
-		
-		if callback then
-			callback()
-		end
-	end)
-end
-
-
-return NotifyCore
+return notifyroblox
